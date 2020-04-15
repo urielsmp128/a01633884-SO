29f92a1d85900d010c01b949d371b874767d469c
Uriel Madrigal <urielsmp128@outlook.com>
2020-04-15 02:06:59 +0000

    practica7

---
+++ signal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+
+
+
+void processSignal(int s){
+    printf("recibi signal %d\n", s);
+    
+}
+
+void dontkillSignal(int m){
+    printf("No me puedes matar %d\n", m);
+    
+}
+
+int main(){
+    
+    signal(10, processSignal);
+    signal(15, dontkillSignal);
+    while (1) {
+    sleep(3);
+    printf("Trabajando\n");
+    }
+
+    return 1;
+   
+}
