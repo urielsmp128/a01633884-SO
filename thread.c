b612d4722b5e88349955ca3a604f126a3f51a2a0
Uriel Madrigal <urielsmp128@outlook.com>
2020-04-28 23:32:44 +0000

    practica8

---
+++ thread.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#define NUMTHREADS 5
+
+int val = 5;
+pthread_mutex_t mutex;
+
+void * hello(void *id){
+    pthread_mutex_lock(&mutex);
+    printf("Hello world, \n");
+    val += val;
+    pthread_mutex_unlock(&mutex);
+};
+
+int main(){
+    pthread_t hilos[NUMTHREADS];
+    for(long i = 0; i < NUMTHREADS; i++){
+        pthread_create(&hilos[i], NULL, hello, (void *)i);
+    }   
+    pthread_exit(0);
+    return 0;
+};
